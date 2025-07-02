776f8f0204d98de9ecf8aae992ec748a4a51b49c
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-07-02 19:26:50 +0530

    eggshop files and folders created

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-w-screen bg-white text-black">
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        
+      </div>
+    </div>
+  );
+}
